43265719b9697d7d48187c0fd1e7d4ee2d6f01dc
xxyomegaxx <yoanyoda@hotmail.com>
2024-04-05 00:10:05 -0400

    Changements miniiii

--- src/front-end/utils/add_game.js
+++ src/front-end/utils/add_game.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var submitButton = document.getElementById('requestNewGame');
     submitButton.addEventListener('click', function() {
         console.log("asked for a new room");
-		socket.send("NEWROOM")
+		socket.send("NEWROOM:")
     });
 });
