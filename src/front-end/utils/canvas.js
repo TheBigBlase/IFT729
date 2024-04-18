50dc906fa8261ebf91b287dd5ce413d599370b4c
violette <violette@bigblase.xyz>
2024-04-18 17:14:53 -0400

    guesser can now see the drawing

--- src/front-end/utils/canvas.js
+++ src/front-end/utils/canvas.js
@@ -4,9 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	socket.onopen = () => {
 		socket.send(`JOIN:${id}:\0`);
-		console.log("Socket connected")
+		console.log("send join to " + id);
 	}
-	console.log("send join to " + id);
 
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
@@ -14,48 +13,53 @@ document.addEventListener("DOMContentLoaded", function() {
     // Variable pour suivre si le bouton de la souris est enfoncé
     let isDrawing = false;
 
-    canvas.addEventListener('mousedown', function(event) {
-        // Le bouton de la souris est enfoncé, commencez à dessiner
-        isDrawing = true;
-        // Commencez à dessiner à la position initiale
-        draw(event);
-    });
+	// TODO
+	// this is disgusting, but i shouldnt be the one doing this :)
+	// Please do your job.
+	if (window.location.href.endsWith("drawer")) {
+		canvas.addEventListener('mousedown', function (event) {
+			// Le bouton de la souris est enfoncé, commencez à dessiner
+			isDrawing = true;
+			// Commencez à dessiner à la position initiale
+			draw(event);
+		});
 
-    canvas.addEventListener('mousemove', function(event) {
-        // Si le bouton de la souris est enfoncé, continuez à dessiner
-        if (isDrawing) {
-            draw(event);
-        }
-    });
+		canvas.addEventListener('mousemove', function (event) {
+			// Si le bouton de la souris est enfoncé, continuez à dessiner
+			if (isDrawing) {
+				draw(event);
+			}
+		});
 
-    canvas.addEventListener('mouseup', function() {
-        // Le bouton de la souris est relâché, arrêtez de dessiner
-        isDrawing = false;
-    });
+		canvas.addEventListener('mouseup', function () {
+			// Le bouton de la souris est relâché, arrêtez de dessiner
+			isDrawing = false;
+		});
 
 
-    function draw(event) {
-        const rect = canvas.getBoundingClientRect();
-        const x = Math.round(event.clientX - rect.left);
-        const y = Math.round(event.clientY - rect.top);
+		function draw(event) {
+			const rect = canvas.getBoundingClientRect();
+			const x = Math.round(event.clientX - rect.left);
+			const y = Math.round(event.clientY - rect.top);
 
-         // Couleur bleue
-        const newColor = [0, 0, 255, 255];
+			// Couleur bleue
+			const newColor = [0, 0, 255, 255];
 
-        // Mettre à jour la couleur du pixel
-        const padding = 3; // pour rendre visible l'endroit cliqué
-        for (let abs = x-padding; abs < x+padding; abs++) {
-            for (let ord = y-padding; ord < y+padding; ord++) {
-                let pixelData = ctx.getImageData(abs, ord, 1, 1);
-                for (let i = 0; i < 4; i++) {
-                    pixelData.data[i] = newColor[i];
-                }
-                // Mettre à jour l'affichage
-                ctx.putImageData(pixelData, abs, ord);
-            }
-        }
+			// Mettre à jour la couleur du pixel
+			const padding = 3; // pour rendre visible l'endroit cliqué
+			for (let abs = x - padding; abs < x + padding; abs++) {
+				for (let ord = y - padding; ord < y + padding; ord++) {
+					let pixelData = ctx.getImageData(abs, ord, 1, 1);
+					for (let i = 0; i < 4; i++) {
+						pixelData.data[i] = newColor[i];
+					}
+					// Mettre à jour l'affichage
+					ctx.putImageData(pixelData, abs, ord);
+				}
+			}
 
-        console.log(`Coordonnées du pixel cliqué : (${x}, ${y})`);
-		socket.send(`PX:${x}:${y}:\0`);
-    }
+			console.log(`Coordonnées du pixel cliqué : (${x}, ${y})`);
+			socket.send(`PX:${x}:${y}:\0`);
+		}
+	}
 });
--- src/front-end/utils/socket.js
+++ src/front-end/utils/socket.js
@@ -1,71 +1,97 @@
-// cors ? 
-
 
 const address = 'ws://localhost:8888';
 let socket = new WebSocket(address);
-// leaving this as example of how to get response
-// TODO get events
-//     win / lose
-//     new guesses
-//     other's guesses
+document.addEventListener("DOMContentLoaded", function () {
+	// leaving this as example of how to get response
+	// TODO get events
+	//     win / lose
+	//     new guesses
+	//     other's guesses
 
-// TODO
+	let canvas, ctx, canvasData, width, height;
 
-function go_to_draw(id) {
-	sessionStorage.setItem("game_id", id);
-}
+	if (window.location.href.endsWith("guesser")) {
+		canvas = document.getElementById('canvas');
+		ctx = canvas.getContext('2d');
+		width = canvas.getBoundingClientRect().width;
+		height = canvas.getBoundingClientRect().height;
+		canvasData = ctx.getImageData(0, 0, width, height);
+	}
 
-function handle_message(msg, id = 0) {
-	let res = msg.split(":");
-	switch (res[0]) {
-		case "PX":
-			console.log("PIXEL x: " + res[1] + " y: "+ res[2]);
-			break;
-		case "MSSG":
-			break;
-		case "WIN":
-			break;
-		case "LOOSE":
-			break;
-		case "ERR":
-			break;
-		case "ROOM":
-			// TODO do that in batch
-			sessionStorage.setItem("id", res[1]);
-			res = res.slice(1, undefined);
+	function store_game_id(id) {
+		sessionStorage.setItem("game_id", id);
+	}
 
-			let ul = document.getElementById("roomList");
-			while (ul.firstChild) {
-				myNode.removeChild(ul.lastChild);
+	function handle_message(msg, id = 0) {
+		let res = msg.split(":");
+		switch (res[0]) {
+			case "PX": {
+				// TODO print on guesser side
+				let x = res[1], y = res[2];
+				// TODO separate files maybe?
+				if (window.location.href.endsWith("guesser")) {
+					ctx.fillRect(x, y, 10, 10);
+					console.log("PIXEL x: " + x + " y: " + y);
+				}
+				break;
 			}
-			for (let elt of res) {
-				ul = document.getElementById("roomList");
+			case "MSSG":
+				// TODO
+				break;
+			case "WIN":
+				// TODO
+				break;
+			case "LOOSE":
+				// TODO
+				break;
+			case "ERR":
+				break;
+			case "ROOM": {
+				// TODO do that in batch
+				sessionStorage.setItem("id", res[1]);
+				res = res.slice(1, undefined);
+				let ul = document.getElementById("roomList");
+				while (ul.firstChild) {
+					ul.removeChild(ul.lastChild);
+				}
+				for (let elt of res) {
+					ul = document.getElementById("roomList");
+					let li = document.createElement("li");
+					let a = document.createElement("a");
+					a.setAttribute("href", "guesser");
+					a.setAttribute("onClick", "store_game_id(" + elt + ")");
+					a.appendChild(document.createTextNode("Room n°" + elt));
+					li.appendChild(a);
+					ul.appendChild(li);
+				};
+				break;
+			}
+			case "CONN":
+				console.log("Connected as player " + res[1]);
+				break;
+			case "DRAWER": {
+				// first is room to go
+				let room = res[1];
 				let li = document.createElement("li");
 				let a = document.createElement("a");
+				let ul = document.getElementById("roomList");
 				a.setAttribute("href", "drawer");
-				a.setAttribute("onClick", "go_to_draw(" + elt + ")");
-				a.appendChild(document.createTextNode("Player " + elt + "'s room"));
+				a.setAttribute("onClick", "store_game_id(" + room + ")");
+				a.appendChild(document.createTextNode("Go to your lobby!"));
 				li.appendChild(a);
 				ul.appendChild(li);
-			};
-			break;
-		case "CONN":
-			let id = sessionStorage.getItem("player_id");
-			if (id != undefined) {
-				socket.send("RECONN:" + id + ":\0");
+				break;
 			}
-			break;
-		default:
-			break;
+			default:
+				break;
+		}
 	}
-}
 
+	socket.onmessage = (msg) => {
+		console.log("got msg: " + msg.data);
+		handle_message(msg.data);
+	}
 
-socket.onmessage = (msg) => {
-	console.log("got res" + msg.data);
-	handle_message(msg.data);
-}
-
-
-socket.onclose = () => {
-}
+	socket.onclose = () => {
+	}
+});
